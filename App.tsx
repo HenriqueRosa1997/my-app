b7623148ff08eea9709111d7eda55c2b9ddc02b8
Henrique Rosa <henriquerodrigues89@hotmail.com>
2026-08-17 00:10:15 -0300

    Implement user registration feature with Redux integration and update navigation

--- App.tsx
+++ App.tsx
@@ -1,47 +1,51 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import Home from './src/app/Home';
-import Login from './src/app/Login';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Header from '@/app/components/Header';
+import Footer from "./src/app/components/Footer";
 
-type RootStackParamList = {
-  Login: undefined;
-  Home: undefined;
-};
 
-const StackNavigation =
-  createNativeStackNavigator<RootStackParamList>();
+import RegisterUser from "./src/app/Register/index";
+import Login from './src/app/Login';
+import { store } from "./src/utils/store";
+import { Provider } from "react-redux";
+
+const StackNavigation = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StackNavigation.Navigator initialRouteName="Login">
+    <Provider store={store}>
+      <NavigationContainer>
+        <StackNavigation.Navigator initialRouteName="Login">
+          <StackNavigation.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+          <StackNavigation.Screen
+            name="Home"
+            component={Home}
+            options={({ navigation }) => ({
+              header: () => <Header title="Home" onMenuPress={() => {}} />,
+            })}
+          />
 
-        <StackNavigation.Screen
-          name="Login"
-          component={Login}
-          options={{
-            headerShown: false,
-          }}
-        />
-
-        <StackNavigation.Screen
-          name="Home"
-          component={Home}
-          options={{
-            header: () => (
-              <Header
-                title="Home"
-                onMenuPress={() => {}}
-              />
-            ),
-          }}
-        />
-
-      </StackNavigation.Navigator>
-    </NavigationContainer>
+          <StackNavigation.Screen
+            name="RegisterUser"
+            component={RegisterUser}
+            options={({ navigation }) => ({
+              header: () => (
+                <Header
+                  title="RegisterUser"
+                  onMenuPress={() => {}}
+                  canGoBack={navigation.canGoBack()}
+                />
+              ),
+                })}
+          />
+        </StackNavigation.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -2,6 +2,9 @@
 import { Container, FooterButton, FooterText } from "./style";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
+import { signOut } from 'firebase/auth';
+import { auth } from '../../../config/firebase';
+import { Text } from 'react-native';
 
 export default function Footer() {
   const router = useRouter();
@@ -10,12 +13,31 @@ export default function Footer() {
     router.push("/Home");
   };
 
-  return (
+  const handleNavigateToRegisterUser = () => {
+    navigation.navigate('RegisterUser');
+  };
+ const handleLogout = async () => {
+    await signOut(auth);
+    navigation.navigate('Login');
+  };
+
+
+   return (
     <Container>
       <FooterButton onPress={handleNavigateToHome}>
         <Ionicons name="home" size={24} color="white" />
-        <FooterText>Home</FooterText>
+        <FooterText><Text>Home</Text></FooterText>
+      </FooterButton>
+      <FooterButton onPress={handleNavigateToRegisterUser}>
+        <Ionicons name="navigate" size={24} color="white" />
+        <FooterText><Text>Register Usuário</Text></FooterText>
+      </FooterButton>
+      <FooterButton onPress={handleLogout}>
+        <Ionicons name="log-out" size={24} color="white" />
+        <FooterText><Text>Logout</Text></FooterText>
       </FooterButton>
     </Container>
   );
 }
+
+
